9f3ccd4c6a387af9c8717f3a3a03ff13dc5119f2
robertopimenta <robertoayres@fiema.org.br>
2021-12-09 20:58:45 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
-const cors = require('cors')
+const cors = require('cors');
+const mongoose = require('mongoose');
 
 const PORT = 5000;
+
+mongoose.connect('mongodb://localhost:27017/Cookmaster', {
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Conexão com o banco de dados realizada com sucesso.');
+}).catch((err) => {
+    console.log(err);
+});
 
 app.use(cors());
 app.use(express.json());
@@ -12,6 +21,11 @@ app.get('/index', (req, res) => {
         mensagem: 'Você acessou a rota /index'
     });
 });
+
+app.post('/novoUsuario', (req, res) => {
+    const dados = req.body;
+    console.log(dados);
+})
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
